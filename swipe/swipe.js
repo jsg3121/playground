32600b212f799c2002ef81c6cbb23760dfa10e24
jsg3121 <xodm95@gmail.com>
2020-03-13 20:42:57 +0900

    swipe_설명 추가

--- swipe/swipe.js
+++ swipe/swipe.js
@@ -1,22 +1,22 @@
 var controlBar = document.getElementById('control_bar'),
-swipeBox = document.getElementById('swipebox'),
-afterBg = document.getElementById('afterBg'),
-swipeVal = false
+    swipeBox = document.getElementById('swipebox'),
+    afterBg = document.getElementById('afterBg'),
+    swipeVal = false
 
 controlBar.addEventListener('mousedown', (e) => {
-swipeVal = true
-swipe(e)
+    swipeVal = true
+    swipe(e)
 })
 
 controlBar.addEventListener('mouseup', (e) => {
-swipeVal = false
+    swipeVal = false
 })
 
 function swipe(e) {
-swipeBox.addEventListener('mousemove', (e) => {
-    if (swipeVal) {
-        controlBar.style.left = (e.clientX - (controlBar.offsetWidth / 2)) + 'px'
-        afterBg.style.width = (e.clientX - (controlBar.offsetWidth / 2)) + 'px'
-    }
-})
+    swipeBox.addEventListener('mousemove', (e) => {
+        if (swipeVal) {
+            controlBar.style.left = (e.clientX - (controlBar.offsetWidth / 2)) + 'px'
+            afterBg.style.width = (e.clientX - (controlBar.offsetWidth / 2)) + 'px'
+        }
+    })
 }
