8ac3023245ffe248a90183a009c4e88d7ccf3fdb
jsg3121 <xodm95@gmail.com>
2021-02-05 18:17:22 +0900

    FEAT(기상청) : api 기상청에서 가져오는 항목 체크

--- weatherApi/src/index.js
+++ weatherApi/src/index.js
@@ -7,11 +7,14 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-let apiKey = "422JryGS9%2B676hcl7wOZ4jh5de2s99vCJr2NcRWV4YXkv9nQP8C0BFGDPVlBt55Fyy5VMJh%2ByRYBMkV%2BcciYZg%3D%3D";
+// 기상청 테스트
 
-// axios.get("http://api.openweathermap.org/data/2.5/forecast?q=London,us&appid=9c1ca1f34e263d5ef31049a8d5e611c9").then((response) => {
-axios.get(`http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?serviceKey=${apiKey}&numOfRows=10&pageNo=1&dataType=JSON&base_date=20210129&base_time=0230&nx=1&ny=1`).then((response) => {
+let apiKey = "422JryGS9%2B676hcl7wOZ4jh5de2s99vCJr2NcRWV4YXkv9nQP8C0BFGDPVlBt55Fyy5VMJh%2ByRYBMkV%2BcciYZg%3D%3D";
+// axios.get(`http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?serviceKey=${apiKey}&numOfRows=10&pageNo=1&dataType=json&base_date=20210205&base_time=1400&nx=59&ny=125`).then((response) => {
+axios.get(`http://apis.data.go.kr/1360000/VilageFcstInfoService/getUltraSrtFcst?serviceKey=${apiKey}&numOfRows=100&pageNo=1&dataType=json&base_date=20210205&base_time=1600&nx=59&ny=125`).then((response) => {
+  // console.log(response.data);
   console.log(response.data.response.body.items);
+  console.log(response.data.response.body.items.item.length);
 });
 
 app.listen(PORT, () => {
