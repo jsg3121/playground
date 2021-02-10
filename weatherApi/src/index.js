3ee5da07b2dde8cd173bd702a91775e581e72488
jsg3121 <xodm95@gmail.com>
2021-02-10 18:32:38 +0900

    FEAT(API) : 대기환경 API 수정

--- weatherApi/src/index.js
+++ weatherApi/src/index.js
@@ -1,5 +1,5 @@
 // openweathermap api ID = 9c1ca1f34e263d5ef31049a8d5e611c9
-// 기상청 api ID = 422JryGS9%2B676hcl7wOZ4jh5de2s99vCJr2NcRWV4YXkv9nQP8C0BFGDPVlBt55Fyy5VMJh%2ByRYBMkV%2BcciYZg%3D%3D
+// 공공API api ID = 422JryGS9%2B676hcl7wOZ4jh5de2s99vCJr2NcRWV4YXkv9nQP8C0BFGDPVlBt55Fyy5VMJh%2ByRYBMkV%2BcciYZg%3D%3D
 
 const express = require("express");
 const axios = require("axios");
@@ -8,13 +8,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // 기상청 테스트
-
 let apiKey = "422JryGS9%2B676hcl7wOZ4jh5de2s99vCJr2NcRWV4YXkv9nQP8C0BFGDPVlBt55Fyy5VMJh%2ByRYBMkV%2BcciYZg%3D%3D";
 // axios.get(`http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?serviceKey=${apiKey}&numOfRows=10&pageNo=1&dataType=json&base_date=20210205&base_time=1400&nx=59&ny=125`).then((response) => {
-axios.get(`http://apis.data.go.kr/1360000/VilageFcstInfoService/getUltraSrtFcst?serviceKey=${apiKey}&numOfRows=100&pageNo=1&dataType=json&base_date=20210205&base_time=1600&nx=59&ny=125`).then((response) => {
+axios.get(`http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?sidoName=%EC%84%9C%EC%9A%B8&pageNo=1&numOfRows=5&returnType=json&serviceKey=${apiKey}`).then((response) => {
   // console.log(response.data);
   console.log(response.data.response.body.items);
-  console.log(response.data.response.body.items.item.length);
 });
 
 app.listen(PORT, () => {
